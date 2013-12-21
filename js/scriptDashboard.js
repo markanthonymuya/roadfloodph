a92e5b72d17d8eef0b0524a09158721a8874110f
Mark Anthony Muya <muya.markanthony@gmail.com>
2013-12-21 11:55:39 +0800

    dashboard showing current hour monitoring
    still some fix must done

--- js/scriptDashboard.js
+++ js/scriptDashboard.js
@@ -2,41 +2,65 @@ var smsUpdateLogs;
 
 $(document).ready(function () {
 
+  var getCurrentDate = new Date();
   var timeFrame = "hour";
   var timelineReference = {};
   var smsLogsJson;
+  var currentTime = getCurrentDate.getHours() + 1;
+
   $("#timelineNav").hide();
 
   smsUpdateLogs = function(){
     $.post("http://roadfloodph.cloudapp.net/roadfloodph/smsLogs.php",{unitSimNumber: "9275628107"}, function (json) {
       smsLogsJson = json;
-      console.log(smsLogsJson);
-      var getCurrentDate = new Date();
+      // console.log(smsLogsJson);
       $.get("http://roadfloodph.cloudapp.net/roadfloodph/timeStamp.php",{year: getCurrentDate.getFullYear()}, function (json) {
         timelineReference = json;
-        console.log(timelineReference);
-        hourTimeline();
+        // console.log(timelineReference);
+        hourTimeline(currentTime);
       });
     });
   };
 
-  var hourTimeline = function(){
-    var currentHour = timelineReference.today;
-    var nextHour = currentHour + 3600;
+  $("#driveRight").click(function(){
+    if(currentTime < 24){
+      currentTime++;
+      hourTimeline(currentTime);
+      console.log(currentTime);
+    }
+  });
+
+  $("#driveLeft").click(function(){
+    if(currentTime > 0){
+      currentTime--;
+      hourTimeline(currentTime);
+      console.log(currentTime);
+    }
+  });
+
+  var hourTimeline = function(hours){
+    var currentDay = timelineReference.today + (3600*(hours));
+    // var currentDay = timelineReference.today + (3600*(hours - 1));
+    var referenceEndpoint = currentDay + 3600;
+    // var referenceEndpoint = timelineReference.tomorrow;
     var hourDataValues = new Array();
     var hourLabelValues = new Array();
     var dataLength = smsLogsJson["generalCounter"];
 
-    console.log("currentHour: " + currentHour);
-    console.log("nextHour: " + nextHour);
+    console.log("currentDay: " + currentDay);
+    console.log("referenceEndpoint: " + referenceEndpoint);
 
     var dataSetQueue = 1;
 
-      for (var j = 0; j < dataLength; j++) {
-        if(smsLogsJson["timestamp"+(j)] >= currentHour || smsLogsJson["timestamp"+(j)] <= nextHour){
+      for (var j = 1; j <= dataLength; j++) {
+
+        // console.log(smsLogsJson["timestamp"+j] >= currentDay);
+        // console.log(smsLogsJson["timestamp"+j] <= referenceEndpoint);
+
+        if(smsLogsJson["timestamp"+j] >= currentDay && smsLogsJson["timestamp"+j] <= referenceEndpoint){
           
           if(dataSetQueue == 1){
-
+            //get previous reading
             if(smsLogsJson["reportedFloodLevel"+(j-1)] == undefined){
               hourDataValues[0] = 0;
             }
@@ -50,15 +74,34 @@ $(document).ready(function () {
           hourLabelValues[dataSetQueue] = smsLogsJson["receivedTime"+(j)];
           dataSetQueue++;
           
+          // it is always replaced by a newer value if it still has a next value
+          // else, if it is the last value, then it will be preserved as the last value to be displayed
+          // with respect to the end of the chosen time.
           hourDataValues[dataSetQueue] = parseFloat(smsLogsJson["reportedFloodLevel"+(j)]);
           hourLabelValues[dataSetQueue] = "01:00";
+        }
+
+        //if there is no results within that timeframe, we'll get the previous value for the first
+        //displayed value and make a constant value up to the last selected end time
+        else if(dataSetQueue == 1){
+            //get previous reading
+            if((smsLogsJson["timestamp"+j] >= currentDay) == false){
+              hourDataValues[0] = parseFloat(smsLogsJson["reportedFloodLevel"+j])-0.0001;
+              hourDataValues[1] = parseFloat(smsLogsJson["reportedFloodLevel"+j]);
+              hourDataValues[2] = hourDataValues[0];
+
+            }
+          
+            hourLabelValues[0] = "00:00";
+            hourLabelValues[1] = "01:30";
+            hourLabelValues[2] = "01:00";
         }
       }
 
     data.datasets[0].data = hourDataValues;
     data.labels = hourLabelValues;
-    console.log(hourDataValues);
-    console.log(hourLabelValues);
+   /* console.log(hourDataValues);
+    console.log(hourLabelValues);*/
 
     var myNewChart = new Chart(ctx).Line(data,options);
   };
