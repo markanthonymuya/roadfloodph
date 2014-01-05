26a30758f9c0c3b67b3d617c959782832c251baa
Mark Anthony Muya <muya.markanthony@gmail.com>
2014-01-05 14:16:24 +0800

    removed wrench settings update button in  default page
    allowing change of settings only at admin page

--- js/scriptRegistration.js
+++ js/scriptRegistration.js
@@ -57,11 +57,15 @@ $(document).ready(function () {
 		//get all units related to the owner
 		$.post("http://roadfloodph.cloudapp.net/roadfloodph/searchUnit.php", {ownerId: "1"}, function (result) {
 			units = result;
+			var stringSettings = "";
+			if(location.href == "http://roadfloodph.cloudapp.net/admin/"){
+				stringSettings = '<button id="editBtn'+i+'" class="btn btn-default pull-right editBtn"><span class="glyphicon glyphicon-wrench"></span></button>';
+			}			
 			if(service='manageUnit'){
 				$("#loadingImage").hide();
 				if(units['generalCounter']>0){
 					for(var i = 1; i <= units['generalCounter']; i++){
-						$("#manageBody").append('<p class="appendedBodyMsg" id="appended'+i+'"><span data-rf="'+i+'" id="unitName'+i+'">'+units['unitName'+i]+'<button id="dashboardBtn'+i+'" style="margin-left: 5px;" class="btn btn-primary pull-right dashboardBtn">Dashboard</button><button type="button" class="btn btn-default pull-right backButton" style="margin-left: 5px;"><span class="glyphicon glyphicon-backward"></span> Back<button id="activateBtn'+i+'" style="margin-left: 5px;" class="btn btn-warning pull-right activateBtn">Activate</button><button id="editBtn'+i+'" class="btn btn-default pull-right editBtn"><span class="glyphicon glyphicon-wrench"></span></button><span id="batteryId'+i+'" class="btn btn-default batteryLabel pull-right" style="margin-right: 5px;"><img class="batteryLabel" src="../assets/ico/battery.png" style="display: inline;" /><span class="batteryLabelText"> '+units["unitPowerLevel"+i]+'</span>%</span><span class="noticeDetails pull-right" style="margin-right: 200px;">Click chart to see detailed view</span></p></span><br class="appendedBodyMsg breaks">');
+						$("#manageBody").append('<p class="appendedBodyMsg" id="appended'+i+'"><span data-rf="'+i+'" id="unitName'+i+'">'+units['unitName'+i]+'<button id="dashboardBtn'+i+'" style="margin-left: 5px;" class="btn btn-primary pull-right dashboardBtn">Dashboard</button><button type="button" class="btn btn-default pull-right backButton" style="margin-left: 5px;"><span class="glyphicon glyphicon-backward"></span> Back<button id="activateBtn'+i+'" style="margin-left: 5px;" class="btn btn-warning pull-right activateBtn">Activate</button>'+stringSettings+'<span id="batteryId'+i+'" class="btn btn-default batteryLabel pull-right" style="margin-right: 5px;"><img class="batteryLabel" src="../assets/ico/battery.png" style="display: inline;" /><span class="batteryLabelText"> '+units["unitPowerLevel"+i]+'</span>%</span><span class="noticeDetails pull-right" style="margin-right: 200px;">Click chart to see detailed view</span></p></span><br class="appendedBodyMsg breaks">');
 						$(".backButton").hide();
 						$(".noticeDetails").hide();
 						$(".batteryLabel").hide();
