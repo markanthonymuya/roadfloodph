5b89cfe279834229887d22fc83a943f0651c7543
Mark Anthony Muya <muya.markanthony@gmail.com>
2014-01-12 07:47:25 +0800

    fixed logic in search Unit.. now i understand

--- roadfloodph/searchUnit.php
+++ roadfloodph/searchUnit.php
@@ -3,6 +3,8 @@
  require('../key/access.php');
 
  $emailAddress = $_POST['emailAddress'];
+ // $emailAddress = "jethdeguzman@outlook.com";
+
 
  if($emailAddress != "public"){
 
@@ -12,69 +14,83 @@
      $ownerId = $owners['ownerId'];
 
      //check if the unit has the same number
-     $result = mysqli_query($con, "SELECT * from unitregistration WHERE ownerId='$ownerId'");
+     $resultUnit = mysqli_query($con, "SELECT * from unitregistration WHERE ownerId='$ownerId'");
 
      $selection = array();
      $generalCounter = 0;
+     $counter = 1;
+     $unitIdList = array();
 
-    while($row = mysqli_fetch_array($result))
+    while($rowUnits = mysqli_fetch_array($resultUnit))
     {
        	$generalCounter++;
-        $unitId = $row['unitId'];
-        $selection['unitId'.$unitId] = $row['unitId'];
+        $unitId = $rowUnits['unitId'];
+        $unitIdList[$counter] = $unitId;
+        $selection['unitId'.$counter] = $unitId;
         $unitList = mysqli_query($con, "SELECT unitCode from unitlist WHERE ownerId='$ownerId' AND unitId='$unitId'");
         $unitCodeDB = mysqli_fetch_array($unitList);
-        $selection['unitCode'.$unitId] = $unitCodeDB['unitCode'];
-        $selection['unitSimNumber'.$unitId] = $row['unitSimNumber'];
-        $selection['unitViewing'.$unitId] = $row['unitViewing'];
-        $selection['unitRegion'.$unitId] = $row['unitRegion'];
-        $selection['unitName'.$unitId] = $row['unitName'];
-        $selection['unitStatus'.$unitId] = $row['unitStatus'];
-        $selection['unitAT'.$unitId] = $row['accessToken'];
-        $selection['unitFreq'.$unitId] = $row['frequency'];
-        $selection['unitSmsCode'.$unitId] = $row['unitSmsCode'];
-        $selection['unitSmsNotif'.$unitId] = $row['unitSmsNotif'];
+        $selection['unitCode'.$counter] = $unitCodeDB['unitCode'];
+        $selection['unitSimNumber'.$counter] = $rowUnits['unitSimNumber'];
+        $selection['unitViewing'.$counter] = $rowUnits['unitViewing'];
+        $selection['unitRegion'.$counter] = $rowUnits['unitRegion'];
+        $selection['unitName'.$counter] = $rowUnits['unitName'];
+        $selection['unitStatus'.$counter] = $rowUnits['unitStatus'];
+        $selection['unitAT'.$counter] = $rowUnits['accessToken'];
+        $selection['unitFreq'.$counter] = $rowUnits['frequency'];
+        $selection['unitSmsCode'.$counter] = $rowUnits['unitSmsCode'];
+        $selection['unitSmsNotif'.$counter] = $rowUnits['unitSmsNotif'];
+        $counter++;
     }
      
     $resultPower = mysqli_query($con, "SELECT unitId, unitPowerLevel from unitpowermonitoring");
+    $counter = 1;
 
-    while($row = mysqli_fetch_array($resultPower))
+    while($rowPower = mysqli_fetch_array($resultPower))
     {
-        $unitId = $row['unitId'];
-        $selection['unitPowerLevel'.$unitId] = $row['unitPowerLevel'];
+        if($rowPower['unitId'] == $unitIdList[$counter]){
+            $selection['unitPowerLevel'.$counter] = $rowPower['unitPowerLevel'];
+        }
+        $counter++;
     }
 
     $selection['generalCounter'] = $generalCounter;
 }
 else{
     //check if the unit has the same number
-     $result = mysqli_query($con, "SELECT * from unitregistration WHERE unitViewing='public' AND unitStatus='ACTIVATED'");
+     $resultUnit = mysqli_query($con, "SELECT * from unitregistration WHERE unitViewing='public' AND unitStatus='ACTIVATED'");
 
      $selection = array();
      $generalCounter = 0;
+     $counter = 1;
+     $unitIdList = array();
 
-    while($row = mysqli_fetch_array($result))
+    while($rowUnits = mysqli_fetch_array($resultUnit))
     {
         $generalCounter++;
-        $unitId = $row['unitId'];
-        $selection['unitId'.$unitId] = $row['unitId'];
-        $selection['unitSimNumber'.$unitId] = $row['unitSimNumber'];
-        $selection['unitViewing'.$unitId] = $row['unitViewing'];
-        $selection['unitRegion'.$unitId] = $row['unitRegion'];
-        $selection['unitName'.$unitId] = $row['unitName'];
-        $selection['unitStatus'.$unitId] = $row['unitStatus'];
-        $selection['unitAT'.$unitId] = $row['accessToken'];
-        $selection['unitFreq'.$unitId] = $row['frequency'];
-        $selection['unitSmsCode'.$unitId] = $row['unitSmsCode'];
-        $selection['unitSmsNotif'.$unitId] = $row['unitSmsNotif'];
+        $unitId = $rowUnits['unitId'];
+        $unitIdList[$counter] = $unitId;
+        $selection['unitId'.$counter] = $unitId;
+        $selection['unitSimNumber'.$counter] = $rowUnits['unitSimNumber'];
+        $selection['unitViewing'.$counter] = $rowUnits['unitViewing'];
+        $selection['unitRegion'.$counter] = $rowUnits['unitRegion'];
+        $selection['unitName'.$counter] = $rowUnits['unitName'];
+        $selection['unitStatus'.$counter] = $rowUnits['unitStatus'];
+        $selection['unitAT'.$counter] = $rowUnits['accessToken'];
+        $selection['unitFreq'.$counter] = $rowUnits['frequency'];
+        $selection['unitSmsCode'.$counter] = $rowUnits['unitSmsCode'];
+        $selection['unitSmsNotif'.$counter] = $rowUnits['unitSmsNotif'];
+        $counter++;
     }
      
     $resultPower = mysqli_query($con, "SELECT unitId, unitPowerLevel from unitpowermonitoring");
+    $counter = 1;
 
-    while($row = mysqli_fetch_array($resultPower))
+    while($rowPower = mysqli_fetch_array($resultPower))
     {
-        $unitId = $row['unitId'];
-        $selection['unitPowerLevel'.$unitId] = $row['unitPowerLevel'];
+        if($rowPower['unitId'] == $unitIdList[$counter]){
+            $selection['unitPowerLevel'.$counter] = $rowPower['unitPowerLevel'];
+        }
+        $counter++;
     }
 
     $selection['generalCounter'] = $generalCounter;
